8f76d8a0867e72e3835fa377c9772f6a68861a28
pdogg <pdogg@0xffe4>
2014-03-27 21:38:22 -0400

    Added jt65sound to calc tones in hz and pocwithtones.py to test

---
+++ jt65sound.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+##some functions to calculate the tone values in hz for jt65 messages
+## key function to call is tonewithsync it does all the work
+
+def tone(number, m=1, offset=0):
+##return a tone in HZ for the input number specified
+#"Each channel symbol generates a tone at frequency 1270.5 + 2.6917 (N+2) m Hz, where N is
+#the integral symbol value, 0 <=N >= 63, and m assumes the values 1, 2, and 4 for JT65 sub-
+#modes A, B, and C."
+
+	return offset + 1270.5 + (2.6917 * (number + 2 ) * m)
+
+def tonepacket(message, m=1, offset=0):
+#takes in a message array and returns an array of tones representing the jt65 audio tones in the message
+
+	output = np.array(range(63),dtype=np.float)
+	for x in range(0,63):
+		output[x] = tone(message[x], m, offset)
+	return output
+
+def toneswithsync(message, m=1, offset=0):
+## take in a jt65 packet and return a full set of tone values, ready to go with sync vector already calcualted in
+# this is HZ ready to covert to audio and put out on the wire
+#m is 1 2 or 4 for submodes a b and c
+#offset is frequency offset
+	output = np.array(range(125),dtype=np.float)
+	synctone = 1270.5 + offset
+	messagetones = tonepacket(message, m, offset)
+	messageindex = 0
+#the mystic 'pseudo-random sequence"
+	syncvector = [1,0,0,1,1,0,0,0,1,1,1,1,1,1,0,1,0,1,0,0,0,1,0,1,1,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,0,1,1,0,1,1,1,1,0,0,0,1,1,0,1,0,1,0,1,1,0,0,1,1,0,1,0,1,0,1,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,0,1,1,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1]
+	for x in range(0,125):
+		if syncvector[x] == 1:
+			output[x] = synctone
+		else:
+			output[x] = messagetones[messageindex]
+			++messageindex
+	return output
+
---
+++ pocwithtones.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import numpy as np
+import jt65wrapy as jt
+import jt65stego as jts
+import jt65sound as jtsound
+import sys
+
+key = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]
+
+if len(sys.argv) < 3:
+	print 'pocwithtones.py -- Proof of concept JT65 steganography encoder'
+	print 'Usage: ./pocwithtones.py <intented jt65 message> <secret message>\n'
+	sys.exit(1)
+	    
+
+
+legitmsg = sys.argv[1]
+secretmsg = sys.argv[2]
+
+legitjt = jt.encode(legitmsg)
+secretjt = jt.encode(secretmsg)
+
+print "\n\n--++== JT65 Stego PoC ==++--"
+print "JT65 legit message 	: " + legitmsg 
+print "Encoded as		: " + str(legitjt)
+
+legitpacket = jt.prepmsg(legitjt)
+print "\nLegit channel symbols with RS:"
+print legitpacket
+
+print "\nSecret message		 : " + secretmsg
+print "Secret message encoded   : " + str(secretjt)
+
+print "Stego with key		: " + str(key)
+
+stegedpacket = jts.jtsteg(legitpacket,secretjt,key)
+
+print "\nSteged channel packet:"
+print stegedpacket
+
+print "\nTones of message:"
+print jtsound.tonepacket(stegedpacket)
+
+print "\nTones to send on wire:"
+print jtsound.toneswithsync(stegedpacket)
+
+recoveredsteg = jts.jtunsteg(stegedpacket,key)
+
+print "\nRecovered Stego message : " + str(recoveredsteg)
+
+print "\nDecoded Stego message : " + jt.decode(recoveredsteg)
+
+print "\n\nDecoded JT65 message : "
+print jt.decode(jt.unprepmsg(stegedpacket))
+print "\n\n"
