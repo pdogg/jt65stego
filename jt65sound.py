ea68999b63dbc15285415c775e9a3dc5139c9a7f
pdogg <pdogg@0xffe4>
2014-03-29 10:35:23 -0400

    Sound sounds good... Need to get rid of the delay gap

--- jt65sound.py
+++ jt65sound.py
@@ -50,39 +50,38 @@ def outputpygame(tones):
 
   bits = 16
 
-  pygame.mixer.pre_init(44100, -bits, 2)
+  pygame.mixer.pre_init(44100, -bits, 1, 1024)
   pygame.init()
   _display_surf = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF)
 
 
-  duration = .372        # in seconds
+  duration = 1     # in seconds
 
   for index in range(0,125):
     #freqency for the left speaker
     frequency_l = tones[index]
     #frequency for the right speaker
-    frequency_r = tones[index]
+    frequency_r = 0
 
     #this sounds totally different coming out of a laptop versus coming out of headphones
 
     sample_rate = 44100
-
     n_samples = int(round(duration*sample_rate))
-    #n_samples = 4096
+ #   n_samples = 4096
 	#setup our numpy array to handle 16 bit ints, which is what we set our mixer to expect with "bits" up above
-    buf = np.zeros((n_samples, 2), dtype = np.int16)
+    buf = np.zeros(n_samples, dtype = np.int16)
     max_sample = 2**(bits - 1) - 1
 
     for s in range(n_samples):
 	  t = float(s)/sample_rate    # time in seconds
 
 	  #grab the x-coordinate of the sine wave at a given time, while constraining the sample to what our mixer is set to with "bits"
-	  buf[s][0] = int(round(max_sample*math.sin(2*math.pi*frequency_l*t)))        # left
-	  buf[s][1] = int(round(max_sample*0.5*math.sin(2*math.pi*frequency_r*t)))    # right
+	  buf[s] = int(round(max_sample*math.sin(2*math.pi*frequency_l*t)))        # left
+#	  buf[s][1] = int(round(max_sample*0.5*math.sin(2*math.pi*frequency_r*t)))    # right
 
 	  sound = pygame.sndarray.make_sound(buf)
 #play once, then loop forever
-    sound.play()
+    sound.play(0, 327)
 
 
 
