d1d386d06ae347ffe00a8f1b0c0afe49349a0e75
Paul Drapeau - @pdogg77 <paul@pauldrapeau.com>
2014-06-05 15:06:14 -0400

    Update jt65.f90
    Added modification note for GPL and FCC reg

--- lib/jt65.f90
+++ lib/jt65.f90
@@ -1,5 +1,21 @@
 program jt65
-
+!This library is derived from the JT65/JT9 etc. library released under GPL as part of the WSJT-X source
+!originally authored by Joe Taylor K1JT and available from: http://www.physics.princeton.edu/pulsar/K1JT/devel.html!
+!Many files have been modified for use in the JT65Stego project in May/June of 2014 by
+!Paul Drapeau and Brent Dukes and this version of the library should not be used for any purpose 
+!other than the study of steganography in these protocols. It should not be considered a reliable replacement
+!for the libraries distributed with WSJT-X.
+!
+!
+!47 CFR §97 - Rules of the Amateur Radio Service
+!
+!Subpart A—General Provisions
+!
+!§97.113 Prohibited transmissions.
+!
+!(a) No amateur station shall transmit:
+!
+!(4) Music using a phone emission except as specifically provided elsewhere in this section; communications intended to facilitate a criminal act; messages encoded for the purpose of obscuring their meaning, except as otherwise provided herein; obscene or indecent words or language; or false or deceptive messages, signals or identification.
 ! Test the JT65 decoder for WSJT-X
 
   parameter (NZMAX=60*12000)
